f7eb32ef593aefae8f2153246d76dbe2a446c792
Rock <rock2tonight@gmail.com>
2009-11-26 19:35:18 +0800

    update replace_key.sh

--- replace_key.sh
+++ replace_key.sh
@@ -1,5 +1,5 @@
-# use Menu(keycode 135) to simulate Pointer_Button2
+# use Menu(keycode 117) to simulate Pointer_Button2
+setxkbmap -layout us
 xkbset m #打开mousekeys开关，使系统能够把敲键事件变为点击事件
 xkbset exp =m #当超出(expire)默认的事件间隔后，仍然保留(=)mousekeys(m)特性
-xmodmap -e 'keycode 135 = Pointer_Button2'
-#xterm
+xmodmap -e 'keycode 117 = Pointer_Button2'
